8c6225047ac1b56f567437d49a189c069edcdd60
IT19165226 <IT19165226@my.sliit.lk>
2022-07-11 22:40:11 +0530

    Bill Division

---
+++ HackerRank/src/prepare/algorithm/implementation/bill_division.java
@@ -0,0 +1,14 @@
+package prepare.algorithm.implementation;
+
+public class bill_division {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void bonAppetit(List<Integer> bill, int k, int b) {
+	    // Write your code here
+
+	}
+}

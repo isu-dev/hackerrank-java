89340b753e44fa8cf18224c0d34777550a27cc8a
IT19165226 <IT19165226@my.sliit.lk>
2022-08-27 23:11:15 +0530

    Drawing Book

--- HackerRank/src/prepare/algorithm/implementation/DrawingBook.java
+++ HackerRank/src/prepare/algorithm/implementation/DrawingBook.java
@@ -4,26 +4,35 @@ public class DrawingBook {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hi");
-
+		System.out.println(pageCount(6, 5));
 	}
 	
 	public static int pageCount(int n, int p) {
 	    // Write your code here
-		int ans = 0, front = 1, back = 0;
+		int ans = 0, front = 0, back = 0;
 		
-		if (p == n)
+		//if the last last page is requested nothing to turn, so return 0
+		//if the first page is requested also nothing to turn, so return 0
+		if ((p == n) || (p == 1))
 			return 0;
-		else if ((p % 2 != 0) && (n - p == 1))
-			return 0;
-		else if (p == 1)
-			return 1;
 		else {
-			front += p / 2;
-			back = 
+			//finding the no. of pages to turn from the front
+			front = (p - 1) / 2 + (p - 1) % 2;
+			
+			if ((n % 2 == 0) && (n - p == 1))
+				return 1;
+			else
+				back = (n - p) / 2; // + (n - p) % 2;
+			
+			System.out.println(front);
+			System.out.println(back);
+			
+			if (front > back)
+				return back;
+			else
+				return front;
 		}
-		
-		return ans;
+
 	}
 
 }
